e8582d15a571bd39cba8df8ae47951ff0f9a86b1
Nikolos123 <Isponec_2>
2023-03-07 20:28:15 +0300

    update

--- sort_comb.py
+++ sort_comb.py
@@ -3,22 +3,26 @@ from decorator import decorator_time
 
 
 @decorator_time  # декоратор для замера времени
-def sort_comb(numbers):
-    i = int(len(numbers) / 1.247)  # устанавливаем значение
-    switch = 1
-    while i > 1 or switch > 0:
-        switch = 0
+def sort_comb(numbers) -> list:
+    i = len(numbers)  # устанавливаем значение
+    shrink = 1.247
+    switch = False
+    while not switch:
+        i = int(i/shrink)
+        if i <= 1:
+            i = 1
+            switch = True
+
         j = 0
         while j + i < len(numbers):
             if numbers[j] > numbers[j + i]:
-                numbers[j], numbers[j + i] = numbers[j + i], numbers[i]  # делаем перестановку
-                switch += 1
+                numbers[j], numbers[j + i] = numbers[j + i], numbers[j]  # делаем перестановку
+                switch = False
             j += 1
-        if i > 1:
-            i = int(i / 1.247)  # вышли из второго цикла и делаем проверку
+
+    return numbers
 
 
 list_of_number = [99, 88, 77, 66, 55, 13, 2, 93]  # список объектов для сортировки
-sort_comb(list_of_number)  # передаем список объектов в метод
-print(list_of_number)  # выводим результат
-
+response = sort_comb(list_of_number)  # передаем список объектов в метод
+print(response)  # выводим результат
